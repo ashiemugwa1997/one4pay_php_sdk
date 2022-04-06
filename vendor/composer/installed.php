a2ea3173eba3655964a008c30cb508bf888e7ef2
ashley zarter <ashleyzarter@gmail.com>
2022-04-06 12:55:52 +0200

    reaADME UPDATE

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4d6244d80ec50bacebdd43b133a30cdcea5fb11c',
+        'name' => 'one4pay/payment',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'one4pay/payment' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4d6244d80ec50bacebdd43b133a30cdcea5fb11c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
